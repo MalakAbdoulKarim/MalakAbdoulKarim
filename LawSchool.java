eb0b4e835f7cbc2ea09dd7974993284b214e73a3
Malak Y <164457476+MalakAbdoulKarim@users.noreply.github.com>
2025-01-14 21:26:58 -0500

    Create LawSchool.java

---
+++ LawSchool.java
@@ -0,0 +1,8 @@
+public class LawSchool extends EducationalInstitution {
+public LawSchool(){
+super(3);
+}
+public String graduationRequirements(){
+return super.graduationRequirements() + " and passing the bar";
+}
+}
